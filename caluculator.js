ee5bbaa92d9680d5a0ee6318fe42fc84b2522e4a
Kyaw-Min-lwin <75528116+Kyaw-Min-lwin@users.noreply.github.com>
2021-09-22 23:41:47 +0630

    Update caluculator.js

--- caluculator.js
+++ caluculator.js
@@ -1,22 +1,22 @@
 // selecting the buttons
-let number = document.querySelectorAll('.number');
+let numbers = document.querySelectorAll('.number');
 let inputField = document.querySelector('input');
 let clearAll = document.querySelector('#AC');
 let deleteButton = document.querySelector('#delete')
 let decimal = document.querySelector('#decimal');
-let sign = document.querySelectorAll('.sign');
+let signs = document.querySelectorAll('.sign');
 let equal = document.querySelector('#equal')
 
 // adding the number to the inpt field (screen)
-for (let i = 0; i < number.length; i++) {
-    number[i].addEventListener('click', () => {
+numbers.forEach(number => {
+    number.addEventListener('click', () => {
         if (inputField.value[0] === '0' && inputField.value.includes('.') === false) {
             inputField.value = '';
         }
-        inputField.value += number[i].innerHTML;
-        // console.log(number[i].innerHTML)
+        inputField.value += number.textContent;
     })
-}
+})
+
 
 // the AC button
 clearAll.addEventListener('click', () => {
@@ -25,32 +25,43 @@ clearAll.addEventListener('click', () => {
 
 // delete the previous number or symbol
 deleteButton.addEventListener('click', () => {
-    // console.log(inputField.value, typeof inputField.value)
-    inputField.value = inputField.value.slice(0, -1)
+    inputField.value = inputField.value.slice(0, -1);
+})
+
+// adding functionality to +, - , / , *;
+let signsStr = '+x-÷'
+signs.forEach(sign => {
+    sign.addEventListener('click', () => {
+        if (signsStr.includes(inputField.value[inputField.value.length - 1])) {
+            inputField.value = inputField.value.slice(0, - 1) + sign.textContent;
+        }
+        else if (inputField.value !== '') {
+            inputField.value += sign.textContent;
+        }
+    })
 })
 
 // add decimal
 decimal.addEventListener('click', () => {
-    if (inputField.value.includes('.') === false) {
-        inputField.value += '.'
+    // the problem is it wont let u include another decimal in a valid position
+    let re = /\+|-|x|÷/;
+    if (signsStr.includes(inputField.value.slice(-1)) === false) {
+        if (inputField.value.match(re)) {
+            let s = inputField.value.split(inputField.value.match(re));
+            s[s.length - 1].includes('.') === false ? inputField.value += '.' : null;
+        }
+        else {
+
+            inputField.value.includes('.') === false ? inputField.value += '.' : null;
+        }
     }
 })
-
-// adding functionality to +, - , / , *;
-for (let i = 0; i < sign.length; i++) {
-    sign[i].addEventListener('click', () => {
-        if (inputField.value !== '' && inputField.value[inputField.value.length - 1] !== '*' &&
-            inputField.value[inputField.value.length - 1] !== '+' && inputField.value[inputField.value.length - 1] !== '-' &&
-            inputField.value[inputField.value.length - 1] !== '÷') {
-            inputField.value += sign[i].innerHTML;
-        }
-    })
-}
 
 // equal sign
 equal.addEventListener('click', () => {
     if (inputField.value !== '') {
         inputField.value = inputField.value.replace(/÷/, '/');
+        inputField.value = inputField.value.replace(/x/, '*');
         inputField.value = eval(inputField.value)
     }
 })
